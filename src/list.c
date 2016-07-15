e2cecedd6a066e58eb43279f4029aee5a48f30b8
xiangyay <yuxiangyang326@gmail.com>
2016-07-15 16:29:50 +0800

    edit list

--- src/list.c
+++ src/list.c
@@ -37,7 +37,31 @@ int isEmpty(SeqList &L)
 	return(L.n == 0)?1:0;
 }
 
+int Search(SeqList &L, DataType x)
+{
+	for(int i = 0; i < L.n; i ++)
+	{
+		if(L.data == x)
+			return i + 1;
+	}
 
+	return 0;
+}
+
+void Copy(SeqList &L1, SeqList &L2)
+{
+	L1.maxSize = L2.maxSize;
+	L1.n = L2.n;
+	if(!L1.data)
+	{
+		L1.data = (DataType *)malloc(sizeof(DataType) * L1.maxSize)
+		if(L1.data == NULL)
+		{
+			printf("malloc error\n");
+			return -1;
+		}
+	}
+}
 
 
 
